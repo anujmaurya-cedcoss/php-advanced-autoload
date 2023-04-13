ef067db5f1235ec90c1fcb6e775bad3dfce1960c
Anuj Maurya <anujmaurya@cedcoss.com>
2023-04-13 11:22:56 +0530

    included spl_autoload_register

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php
-    function __autoload($class_name) 
-    {
+    function my_autoloader  ($class_name){
         require_once('./classes/'.$class_name.'.php');
     }
+    spl_autoload_register('my_autoloader');
     $myClass = new classA('hello world!');
     $myClass -> displayVar();
 ?>
